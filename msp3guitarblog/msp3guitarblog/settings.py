c4e13f0c689e146186aa9971b6a701547d573efb
JamesBirchall-dev <jamesbdorel@gmail.com>
2026-02-26 11:25:38 +0000

    debug to false prior to deployment

--- msp3guitarblog/msp3guitarblog/settings.py
+++ msp3guitarblog/msp3guitarblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".codeinstitute-ide.net",
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'msp3guitarblog',
 ]
 
 MIDDLEWARE = [
