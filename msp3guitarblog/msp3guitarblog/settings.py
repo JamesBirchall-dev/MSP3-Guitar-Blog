2adbe78788fe4e89d020c610cc4374179b2aab9c
JamesBirchall-dev <jamesbdorel@gmail.com>
2026-02-26 12:44:36 +0000

    Configure and Connect ProgreSQL

--- msp3guitarblog/msp3guitarblog/settings.py
+++ msp3guitarblog/msp3guitarblog/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env  # noqa: F401
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,12 +90,17 @@ WSGI_APPLICATION = 'msp3guitarblog.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get(
+            "DATABASE_URL",
+            (
+                "postgresql://neondb_owner:npg_fDAQoBtc19uy@"
+                "ep-calm-king-aghtgcbv.c-2.eu-central-1.aws.neon.tech/"
+                "gift_onion_fend_100192"
+            )
+        )
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
